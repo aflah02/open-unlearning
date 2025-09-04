2152cf6c6750784bd9ab15b6aedbd8cf9afe7cfc
Mohammad Aflah Khan <aflah20082@iiitd.ac.in>
2025-09-04 10:57:26 +0200

    Plots

--- run_mia_basic.sh
+++ run_mia_basic.sh
@@ -12,19 +12,19 @@ fi
 
 # List of models to loop over
 MODELS=(
-  "hubble-1b-500b_toks-perturbed-hf"
-  "hubble-1b-500b_toks-standard-hf"
-  "hubble-8b-500b_toks-standard-hf"
+  # "hubble-1b-500b_toks-perturbed-hf"
+  # "hubble-1b-500b_toks-standard-hf"
+  # "hubble-8b-500b_toks-standard-hf"
   "hubble-8b-500b_toks-perturbed-hf"
 )
 
 # List of tasks to loop over
 TASKS=(
-  "mia_gutenberg_popular"
-  "mia_gutenberg_unpopular"
-  "mia_passages_wikipedia"
-  "mia_yago_biographies"
-  "mia_mmlu"
+  # "mia_gutenberg_popular"
+  # "mia_gutenberg_unpopular"
+  "mia_passage_wikipedia"
+  # "mia_yago_biographies"
+  # "mia_mmlu"
 )
 
 # Loop through tasks
